9db705e6fc6be2b61036bcdc123d052ed0563619
Prash2002 <56592188+Prash2002@users.noreply.github.com>
2020-04-15 17:06:42 +0530

    added UI
    registeration page UI added

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,46 +1,135 @@
+
+
+import 'dart:io';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:covidate/screens/userProfile/background.dart';
-import 'package:covidate/services/auth.dart';
-import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:covidate/screens/authenticate/signup.dart';
 import 'package:covidate/shared/loading.dart';
-class SignIn extends StatefulWidget {
-    final Function toggleView;
- SignIn({this.toggleView});
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:covidate/services/auth.dart';
+class Register extends StatefulWidget {
+  final Function toggleView;
+  Register({this.toggleView});
   @override
-  _SignUpState createState() => _SignUpState();
+  _RegisterState createState() => _RegisterState();
 }
 
 String email ='';
  String password = '';
-  String error ='';
-  
-class _SignUpState extends State<SignIn> {
+ String name = ' ';
+int  age = 18 ;
+ String gender;
+ String bio = '';
+ String error ='';
+ bool loading = false;
+ String url;
+class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
-   final _formkey = GlobalKey<FormState>();
-   bool loading = false;
+  final _formkey = GlobalKey<FormState>();
+  final List<String> genders = ['MALE' , 'FEMALE' ,'OTHERS'];
+ File _image;
+
   @override
   Widget build(BuildContext context) {
+
+ 
+
+Future  uploadImage () async{
+final StorageReference postImgref = FirebaseStorage.instance.ref().child('Post Images');
+var timeKey = new DateTime.now();
+final StorageUploadTask uploadTask = postImgref.child(timeKey.toString()+'.jpg').putFile(_image);
+var imgurl = await(await uploadTask.onComplete).ref.getDownloadURL();
+url = imgurl.toString();
+print(url);
+ }
+ Future  getImage() async {
+ var image = await ImagePicker.pickImage( source : ImageSource.gallery);
+   setState((){
     
-    return loading ? Loading() :Scaffold(
-     
-    //  appBar: AppBar(
-    //   backgroundColor : Colors.pink[800],
-    //   title : Text('Sign-In'),
-    //   actions :<Widget>[
-    //     FlatButton.icon(onPressed: (){
-    //       widget.toggleView();
-    //     }, icon: Icon(Icons.person), label: Text('Register')),
-    //   ],),
+    _image = image;
+    
+ });
+ 
+}
+   return loading ? Loading() :  GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child : Scaffold(
+
+        
+   
+//  appBar: AppBar(
+//       backgroundColor : Colors.orange[800],
+//       title : Text('Sign-Up'),
+//       actions :<Widget>[
+//         FlatButton.icon(onPressed: (){
+//           widget.toggleView();
+//         }, icon: Icon(Icons.person), label: Text('Sign-In')),
+//       ]
+
+//     ),
     body: SingleChildScrollView(
-          child: Container(
-        color: Colors.white,
-        padding: EdgeInsets.fromLTRB(20.0, 150.0, 10.0 , 5.0),
-        child : Form(
-          key: _formkey,
-           child : Column(
-          children: <Widget> [
-            SizedBox(height: 20.0),
-           TextFormField(
-             decoration: InputDecoration(
+       
+      // padding: EdgeInsets.symmetric(vertical : 20.0 , horizontal : 50.0),
+      child : IntrinsicHeight(
+        child : Container(
+          color: Colors.white,
+        padding: EdgeInsets.symmetric(vertical : 20.0 , horizontal : 20.0),
+      //     decoration: BoxDecoration(
+       
+      //   gradient : LinearGradient(colors: [Colors.orange[400],Colors.orange[900],Colors.orange,Colors.red[700],Colors.red[900]],
+      //    begin : Alignment.topCenter,
+      //   end : Alignment.bottomCenter,
+      //   stops: [0.2,0.4,0.6,0.8,1.0])
+      // ),
+      child : Form(
+        key: _formkey,
+         child : Column(
+        children: <Widget> [
+        
+          SizedBox(height: 20.0),
+              Align(
+                alignment : Alignment.center,
+                child : CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.grey[400],
+                  child:  ClipOval(
+                    
+                    child : SizedBox(
+                    width : 180,
+                     height : 180,
+                     
+                     child:(_image!=null)? Image.file( _image,fit : BoxFit.fill): Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTiJiDlfMaWW0ATIedRlCUByLXqJvRhLHrzdhDTNSYWx_73D_o6&usqp=CAU',fit:BoxFit.fill),
+                  ),
+                  ),
+                
+                  ),
+              ),
+            SizedBox(height: 15.0),
+         Row(
+           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children : <Widget>[
+              FlatButton.icon(icon: Icon( Icons.camera, size: 30.0),label: Text('Choose Image',style: TextStyle(color: Colors.pink
+               ),), onPressed: (){
+                getImage();
+              }),
+              FlatButton.icon( label: Text('Upload Image', style: TextStyle(color: Colors.pink
+               ),),icon : Icon(Icons.file_upload),onPressed :() async { await uploadImage();} )
+              ],),
+            Expanded(child: 
+         TextFormField(
+           decoration: InputDecoration(
                labelText: 'Enter Email',
                labelStyle: TextStyle(
                  color: Colors.pink
@@ -50,37 +139,102 @@ class _SignUpState extends State<SignIn> {
                  borderRadius: BorderRadius.circular(50.0)
                )
              ),
-             validator: (val) => val.isEmpty ? 'Enter an Email':null,
-             onChanged: (val)
-             {
-              setState(() => email = val);
-             }),
-             
-
-           
-           SizedBox(height: 20.0),
-           TextFormField(
-             decoration: InputDecoration(
-               labelText: 'Enter Password',
-               prefixIcon: Icon(Icons.lock, color: Colors.pink ),
+           validator: (val) => val.isEmpty ? 'Enter an Email':null,
+           onChanged: (val)
+           {
+            setState(() => email = val);
+           }),
+            ),
+         SizedBox(height: 20.0),
+         Expanded(child: 
+         TextFormField(
+           decoration: InputDecoration(
+               labelText: 'Enter password',
                labelStyle: TextStyle(
                  color: Colors.pink
                ),
+               prefixIcon: Icon(Icons.lock, color: Colors.pink,),
                border: OutlineInputBorder(
                  borderRadius: BorderRadius.circular(50.0)
                )
              ),
-             validator: (val) => val.length < 6 ? 'Enter a password of atleast 6 characters':null,
-             obscureText: true,
-             onChanged : (val){
-               setState(() =>
-                 password=val
-          );
+           validator: (val) => val.length < 6 ? 'Enter a password of atleast 6 characters':null,
+           obscureText: true,
+           onChanged : (val){
+             setState(() =>
+               password=val
+        );
 
-             },
+           },
+         ),),
+         SizedBox(height : 20.0),
+         Expanded(child: 
+           TextFormField(
+            decoration: InputDecoration(
+               labelText: 'Enter Name',
+               labelStyle: TextStyle(
+                 color: Colors.pink
+               ),
+              // prefixIcon: Icon(Icons.person, color: Colors.pink,),
+               border: OutlineInputBorder(
+                 borderRadius: BorderRadius.circular(50.0)
+               )
+             ),
+           validator: (val) => val.isEmpty ? 'Enter a valid name':null,
+           onChanged: (val)
+           { 
+            setState(() => name = val);
+           }),),
+         SizedBox(height: 20.0),
+         Expanded(child: Row(
+           children : <Widget>[
+             Text('Age'),
+             SizedBox(width : 5.0),
+          Slider(
+             value: ( age ?? 18.0).toDouble(),
+             activeColor: Colors.pink,
+             min : 18.0,
+             max : 30.0,
+             divisions: 12,
+           onChanged: (val)
+           {
+            setState(() => age = val.round());
+           }
            ),
-           SizedBox(height : 20.0),
-           GestureDetector(
+           SizedBox(width : 5.0),
+           Text('$age'),]),),        
+            SizedBox(height: 20.0),
+           DropdownButtonFormField(
+            value: gender ?? 'MALE',
+            items: genders.map((gen){
+           return DropdownMenuItem(value: gen,child: Text(gen));
+          }).toList(),
+          
+           onChanged: (val)
+           {
+            setState(() => gender = val);
+           }),
+         SizedBox(height: 20.0),
+           Expanded(
+             child :TextFormField(
+               decoration: InputDecoration(
+               labelText: 'Enter Bio',
+               labelStyle: TextStyle(
+                 color: Colors.pink
+               ),
+               //prefixIcon: Icon(Icons.person, color: Colors.pink,),
+               border: OutlineInputBorder(
+                 borderRadius: BorderRadius.circular(50.0)
+               )
+             ),
+           validator: (val) => val.isEmpty ? 'Enter a small bio':null,
+           onChanged: (val)
+           {
+            setState(() => bio = val);
+           }),),
+         SizedBox(height: 20.0),
+         Expanded(child: 
+          GestureDetector(
              child: Container(
                width: MediaQuery.of(context).size.width*0.8,
                
@@ -94,43 +248,40 @@ class _SignUpState extends State<SignIn> {
                child: Text('Login' , style: TextStyle(color: Colors.white), textAlign: TextAlign.center,),
              ), 
            ),
-                
-             
-             onTap:() async {
-                  if(_formkey.currentState.validate())
-              { setState(() {
-                loading =true;
-              });
-                 dynamic result = await _auth.signInwithEmailandPassword(email, password);
-             
-              if(result == null)
-              {
-               
-                setState(() {
-                   loading = false;
-                  error = 'Could not sign-in with those credentials';
-                });
-              }
-              
-              }
-             },),
-           
-           FlatButton(
-             child: Text("Don't have an account? Register Now"),
+         onTap:() async {
+           if(_formkey.currentState.validate())
+          { setState(() {
+            loading =true;
+          });
+             dynamic result = await _auth.registerwithEmailandPassword(email, password,name,age,bio,gender,url);
+             loading = true;
+          if(result == null)
+          {
+            
+            setState(() {
+               loading = false;
+              error = 'Please supply a valid credentials !!  ';
+            });
+          }
+          }
+
+         },),),
+         FlatButton(
+             child: Text("Already have an account? Login now"),
              onPressed: (){
             widget.toggleView();
           } ,
            ),
-           SizedBox(height : 12.0),
-           Text(error,
-           style : TextStyle(
-             color:  Colors.black,
-             fontWeight : FontWeight.bold,
-             fontSize : 15.0,
+         SizedBox(height : 12.0),
+         Expanded(child: 
+         Text(error,
+         style : TextStyle(
+           color:  Colors.black,
+           fontWeight : FontWeight.bold,
+           fontSize : 15.0,
 
-           ),),
-
-             Stack(
+         ),)),
+         Stack(
               alignment: Alignment.bottomLeft,
               children: <Widget>[
                 WavyFooter(),
@@ -138,15 +289,14 @@ class _SignUpState extends State<SignIn> {
                 CircleYellow(),
               ],
             )
-          ]
+        ]
 
-        ),
+      )
 
-        ),),
-    )
-    
+      ))
+    ),
    
       
-    );
+    ) ),);
   }
-}
+  }
