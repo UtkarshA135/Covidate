f8ee6ef750fd888c2643e5fde31fafcf7d7662e4
Prash2002 <56592188+Prash2002@users.noreply.github.com>
2020-04-27 16:55:51 +0530

    Final UI

--- lib/screens/home/all_users_screen.dart
+++ lib/screens/home/all_users_screen.dart
@@ -8,6 +8,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:neumorphic/neumorphic.dart';
+
 
 class AllUsersScreen extends StatefulWidget {
   _AllUsersScreenState createState() => _AllUsersScreenState();
@@ -57,11 +60,47 @@ class _AllUsersScreenState extends State<AllUsersScreen> {
     
     return Scaffold(
         appBar: AppBar(
-          title: Text("All Users"),
-         
+          // title: Text("All Users"),
+          title: Text(" Chat with Match",
+            style: TextStyle(
+              fontFamily: 'Lobster',
+              color: Colors.black,
+              fontSize: ScreenUtil().setSp(70.0),
+            ),
+          ),
+           centerTitle: true,
+          backgroundColor:  Color(0xFF5AEAF1),
         ),
         body: usersList != null
-            ? Container(
+             ? (usersList.length==0?
+              Container(
+             color: Color(0xEDD8E2),
+              // color: Color(0xfadcf8),
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Sorry!',
+                    style: TextStyle(
+                      color: Colors.redAccent,
+                      fontSize: ScreenUtil().setSp(100.0),
+                      fontFamily: 'Lobster'
+                    )
+                  ),
+                  SizedBox(height: 20.0),
+                  Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQge3zH1vQU2BtGQLBTEeyYY7oY15AXTufAT1EbnKZqbooIfsjI&usqp=CAU'),
+                  SizedBox(height: 30.0),
+                  Text('No matches yet :(',
+                  style: TextStyle(
+                    fontSize: ScreenUtil().setSp(60.0),
+                    letterSpacing: 1.1,
+                    color: Colors.black45,
+                  )
+                  )
+                ],
+              ),
+            )
+           : Container(
                 child: ListView.builder(
                   itemCount: usersList.length,
                   itemBuilder: ((context, index) {
@@ -74,10 +113,19 @@ class _AllUsersScreenState extends State<AllUsersScreen> {
                     {
                       print(usersList[index].data['name']);
                       print(usersList[index].data['ownerId']);
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: usersList[index].data['url']!= null ?
-                            NetworkImage(usersList[index].data['url']): AssetImage('assets/popo.jpg')
+                     return NeuCard(
+                      margin: EdgeInsets.all(10.0),
+                      padding: EdgeInsets.all(5.0),
+                      curveType: CurveType.concave,
+                       bevel: 12,
+                       decoration: NeumorphicDecoration(
+                       color: Colors.white,
+                         borderRadius: BorderRadius.circular(8)
+                       ),
+                          child: ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: usersList[index].data['url']!= null ?
+                              NetworkImage(usersList[index].data['url']): AssetImage('assets/images.png')
                       ),
                       title:usersList[index].data['name'] != null ?Text(usersList[index].data['name'],
                        style: TextStyle(
@@ -103,11 +151,13 @@ class _AllUsersScreenState extends State<AllUsersScreen> {
                                     receiverUid:usersList[index].data[
                                         'ownerId'])));
                       }),
-                    );}
-                  }),
-                ),
-              )
-            : Center(
+                    
+                          ),
+                          );
+                     }} )
+                )
+                          ))
+                      : Center(
                 child: CircularProgressIndicator(),
               ));
   }
