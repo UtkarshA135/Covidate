9a22a28c34cc83d19c89402d0ed306b5961c1c48
Prash2002 <56592188+Prash2002@users.noreply.github.com>
2020-04-27 16:57:42 +0530

    Update card_tile.dart

--- lib/screens/home/card_tile.dart
+++ lib/screens/home/card_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:covidate/models/cards.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class CardTile extends StatelessWidget {
   final Cards card;
   final bool showlike;
@@ -8,25 +9,88 @@ class CardTile extends StatelessWidget {
   Widget build(BuildContext context) {
  return   Stack(children: <Widget>[
                             Container(
-                          width: 700.0,
-                          height: 300.0,
+                          // width: 700.0,
+                          // height: 300.0,
+                          width: ScreenUtil().setWidth(1000.0),
+                         height: ScreenUtil().setHeight(900.0),
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10.0),
                               image: DecorationImage(
-                                  image: NetworkImage(card.url),
+                                  // image: NetworkImage(card.url),
+                                  image: card.url ==null? AssetImage('assets/Rohit.jpg'):NetworkImage(card.url),
                                   fit: BoxFit.cover)),
-                                   child: Column(children: <Widget>[
+                      //              child: Column(children: <Widget>[
                          
-                                    Expanded(child:   Align(  alignment: FractionalOffset.bottomCenter, child:  Text(card.name,
-                                   style : TextStyle(
-                                    // fontFamily: 'IndieFlower',
-                                      fontWeight : FontWeight.bold,
-                                      color:  Colors.red,
-                                      fontSize: 40.0, 
-                                    )
-                                   ),),)
-                       ],)
-                        ),
+                      //               Expanded(child:   Align(  alignment: FractionalOffset.bottomCenter, child:  Text(card.name,
+                      //              style : TextStyle(
+                      //               // fontFamily: 'IndieFlower',
+                      //                 fontWeight : FontWeight.bold,
+                      //                 color:  Colors.red,
+                      //                 fontSize: 40.0, 
+                      //               )
+                      //              ),),)
+                      //  ],)
+                      child: Column(
+                                     children: <Widget>[
+                                       Row(children: <Widget>[
+                                       Expanded(
+                                         child:   Align(  
+                                        alignment: FractionalOffset.topLeft,
+                                        child:  Text(
+                                          // card.name+' , '+ 
+                                          card.age.toString(),
+                                       style : TextStyle(
+                                        // fontFamily: 'IndieFlower',
+                                          fontWeight : FontWeight.bold,
+                                          color:  Colors.red,
+                                         fontSize: 40.0, 
+                                       )
+                                       ),
+
+                                       ),),
+                                       Container(
+                                         color: Colors.black,
+                                         padding: EdgeInsets.symmetric(horizontal: ScreenUtil().setWidth(50.0), vertical: ScreenUtil().setHeight(30.0)),
+                                         child:   Align(  
+                                        alignment: FractionalOffset.topRight,
+                                       child:  Text(
+                                          card.name, 
+                                          // card.age.toString(),
+                                       style : TextStyle(
+                                        // fontFamily: 'IndieFlower',
+                                        fontFamily: 'Lobster',
+                                          // fontWeight : FontWeight.bold,
+                                          color:  Colors.white,
+                                          fontSize: ScreenUtil().setSp(55.0), 
+                                        )
+                                       ),
+
+                                       ),)
+                       ],),
+                                     Expanded(
+                                        child: Align(  
+                                          alignment: FractionalOffset.bottomCenter,
+                                          child:  Container(
+                                            width: ScreenUtil().setWidth(1000.0),
+                                            color: Colors.white,
+                                            child: Text(
+                                         card.bio, 
+                                        textAlign: TextAlign.center,
+                                         // card.age.toString(),
+                                         style : TextStyle(
+                                         fontFamily: 'Pacifico',
+                                        //  fontWeight : FontWeight.bold,
+                                         color:  Colors.black,
+                                         fontSize: ScreenUtil().setSp(75.0), 
+                                            )
+                                         ),
+                                          ),
+
+                                         ),
+                                     )
+                                     ],
+                                   )
+                       ),
                  
                           ],);
     
